15642e15ff115e3586bb229e4a175822c7dad259
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2025-05-26 10:13:47 +0200

    añadidos estilos preliminares de bootstrap

--- src/pages/books/BookList.jsx
+++ src/pages/books/BookList.jsx
@@ -4,6 +4,8 @@ import img1 from "../../assets/img1.jpg"
 import img2 from "../../assets/img2.jpg"
 import img3 from "../../assets/img3.jpg"
 
+import Card from 'react-bootstrap/Card';
+
 function BookList() {
   return (
     <div>
@@ -12,12 +14,14 @@ function BookList() {
 
       <div>
 
-        <div>
-          <img src={img1} alt="cover" width={120} height={200}/>
-          <h3>Book1</h3>
-          <p>Lorem ipsum, dolor sit amet</p>
-          <Link to="/book/details/1">See Book 1</Link>
-        </div>
+        <Card style={{ width: '200px' }}>
+          <Card.Img src={img1} alt="cover" variant="top"/>
+          <Card.Body>
+            <Card.Title>Book1</Card.Title>
+            <Card.Text>Lorem ipsum, dolor sit amet</Card.Text>
+            <Card.Link as={Link} to="/book/details/1">See Book 1</Card.Link>
+          </Card.Body>
+        </Card>
 
         <div>
           <img src={img2} alt="cover" width={120} height={200}/>
